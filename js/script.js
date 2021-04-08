a7788f31838295512a716188bf6c495fdd43ba0f
ismailovD <ismailovdier551@gmail.com>
2021-04-08 17:50:02 +0500

    change title

--- js/script.js
+++ js/script.js
@@ -5,24 +5,36 @@ let prorgess = document.querySelectorAll('.story__skill-percent'),
 prorgess.forEach(el => {
     let text = el.getAttribute('data-percent'),
         i = 0,
-        bar = document.querySelector(`[aria-valuenow="${text}"]`),
-        speed = 1000;
+        bar = document.querySelector(`[aria-valuenow="${text}"]`);
     bar.style.width = '0';
     el.innerHTML = i;
 
     upgrade();
 
     function upgrade() {
-        
+
         if (i <= text) {
             el.innerHTML = i;
             bar.style.width = `${i}%`;
             i++;
-            setTimeout(upgrade, text/1.8);
+            setTimeout(upgrade, text / 1.8);
         }
     }
 
-})  
+})
+ 
+        
+        
+ let map = new google.maps.Map(document.querySelector('#map'));
+console.log(map);
+    var marker = new google.maps.Marker({
+        map:map,
+        position: new google.maps.LatLng(49.72964961473178, 11.395572544039274),  
+        icon: '../images/contact/marker.png'
+    })
+
+    
+
 
 
 
